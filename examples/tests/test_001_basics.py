c4fde16f16e5198d9510cc849d2a89f1ad4e1275
Jesse Mather <jmather@arista.com>
2018-01-09 09:42:53 -0800

    enable example config change

--- examples/tests/test_001_basics.py
+++ examples/tests/test_001_basics.py
@@ -12,7 +12,7 @@ import re
 import pytest
 import arcomm
 
-def test_version(sessions, sdconfig, testconfig):
+def test_version(sessions, sdconfig, testconfig, sdreportsection):
     """Autotest will scan the class for any methods that start with 'test'."""
 
     version = testconfig["software_version"]
@@ -22,11 +22,13 @@ def test_version(sessions, sdconfig, testconfig):
         assert version in str(r[0].output), \
             "Software version should be {}".format(version)
 
+    sdreportsection.append("link", "http://httpbin.org/", text="httpbin", title="link to httpbin")
+
 def test_bogus(sessions, sdconfig, testconfig):
     """Run a bogus command. should throw error"""
     with pytest.raises(arcomm.ExecuteFailed):
         response = sessions.send(r"dut", "show bogus")
 
-# def test_config(sessions):
-#     """test if rollback is triggered"""
-#     sessions.send(r"dut", ["configure", "username timmy nopassword", "end"])
+def test_config(sessions):
+    """test if rollback is triggered"""
+    sessions.send(r"dut", ["configure", "username timmy nopassword", "end"])
