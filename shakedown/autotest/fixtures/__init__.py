db337c63d14555ad43ecc7996e5e941803b9231f
Jesse Mather <jmather@arista.com>
2018-02-02 13:25:32 -0800

    removed '*'

--- shakedown/autotest/fixtures/__init__.py
+++ shakedown/autotest/fixtures/__init__.py
@@ -2,6 +2,6 @@
 # Copyright (c) 2017 Arista Networks, Inc.  All rights reserved.
 # Arista Networks, Inc. Confidential and Proprietary.
 
-from .general import *
-from .auto import *
-from .reporting import *
+from .general import sdconfig, sessions, testconfig
+from .auto import _auto_monkeypatch_send, _auto_rollback, _auto_load_module
+from .reporting import sdreport, sdreportsection
