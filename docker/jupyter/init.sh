a03cb78f97ae9f53ef18f6bbc33e82720314d2c4
Jesse Mather <jmather@arista.com>
2018-07-25 11:23:38 -0700

    add docker files

---
+++ docker/jupyter/init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+CWD=$PWD
+cd /shakedown
+python3 setup.py develop
+cd $CWD
+
+jupyter notebook --NotebookApp.token='' --allow-root --port=8888 --no-browser --ip=0.0.0.0
---
+++ docker/jupyter/ipython_config.py
@@ -0,0 +1,1 @@
+c.InteractiveShellApp.extensions = ['shakedown.ipython.magics']
