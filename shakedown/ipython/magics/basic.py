b40dea0e9368c1625f3599fa57f38107d3748a60
Jesse Mather <jmather@arista.com>
2018-07-25 11:24:43 -0700

    formatting

--- shakedown/ipython/magics/basic.py
+++ shakedown/ipython/magics/basic.py
@@ -63,8 +63,8 @@ class BasicMagics(Magics):
         help="Read from configuration section")
     @line_cell_magic
     def sdconnect(self, line, cell=None, local_ns={}):
-        warnings.warn("`sdconnect` is deprecated. connections are automatically made from `sdconfig`")
-
+        warnings.warn("`sdconnect` is deprecated. connections are \
+                      automatically made from `sdconfig`")
 
     @needs_local_scope
     @magic_arguments.magic_arguments()
@@ -86,10 +86,11 @@ class BasicMagics(Magics):
         template = jinja2.Template(cell)
         commands = template.render(config).splitlines()
 
-        for response in sessions.send(args.endpoints, commands, encoding=args.encoding):
+        for response in sessions.send(args.endpoints, commands,
+                                      encoding=args.encoding):
             if args.output_file:
                 util.plush("Writing output to {}".format(args.output_file))
                 with open(args.output_file, "w") as fh:
-                    fh.write(str(response))
+                    fh.write(str(response) + "\n")
             else:
-                util.plush(str(response))
+                util.plush(str(response) + "\n")
