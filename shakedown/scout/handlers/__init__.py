f334a76d0ed807f1e8872bf3cbc69289564cf7fb
Jesse Mather <jmather@arista.com>
2018-09-20 09:12:52 -0700

    add static route handler

--- shakedown/scout/handlers/__init__.py
+++ shakedown/scout/handlers/__init__.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2018 Arista Networks, Inc.  All rights reserved.
 # Arista Networks, Inc. Confidential and Proprietary.
-from . import bgp, interfaces, system, lldp, lag
+from . import bgp, interfaces, system, lldp, lag, routes
 handlers = [
     ("system", system),
     ("interfaces", interfaces),
     ("bgp", bgp),
     ("lldp", lldp),
-    ("lag", lag)
+    ("lag", lag),
+    ("routes", routes)
 ]
