77f81a6e421a73276155bd5dd400f2d0b9cd859d
Jesse Mather <jmather@arista.com>
2018-09-21 20:30:07 -0700

    include new fixtures

--- shakedown/autotest/fixtures/__init__.py
+++ shakedown/autotest/fixtures/__init__.py
@@ -2,11 +2,11 @@
 # Copyright (c) 2017 Arista Networks, Inc.  All rights reserved.
 # Arista Networks, Inc. Confidential and Proprietary.
 
-from .general import sdconfig, sessions, scout, testconfig, dut, sdut
+from .general import sdconfig, sessions, scout, testconfig, dut, sdut #, procplot
 from .auto import _auto_monkeypatch_send, _auto_rollback, _auto_load_module
-from .auto import _auto_backdoor
+from .auto import _auto_backdoor, _auto_install_arstat, _auto_output_dir
 from .reporting import sdreport, sdreportsection
-
+from .backdoor import procplot
 
 # legacy
 from .reporting import sdreportsection as reportitem
