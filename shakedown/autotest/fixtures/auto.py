2abdfc6f33226c353e340e433886d15a64ee1a7c
Jesse Mather <jmather@arista.com>
2018-09-21 20:28:50 -0700

    push arstat script at start of test run

--- shakedown/autotest/fixtures/auto.py
+++ shakedown/autotest/fixtures/auto.py
@@ -8,9 +8,16 @@ import datetime
 import os
 import warnings
 import yaml
+import subprocess
+import shlex
+import time
+import pexpect
+from pexpect import pxssh
 
 from shakedown.autotest.report import report_store
 from shakedown.autotest import util
+from shakedown.util import mkdir
+from shakedown.autotest.constants import SSH_INIT_REGEX, BACKDOOR_CREDENTIALS
 
 from pprint import pprint
 
@@ -23,6 +30,13 @@ def _auto_load_module(request, sdconfig):
     if "settings" in header:
         sdconfig.merge("settings:\n  {}".format(header["settings"]))
 
+@pytest.fixture(scope="module", autouse=True)
+def _auto_output_dir(request):
+    output_dir = request.config.getoption("output_dir")
+    if output_dir:
+        output_dir = os.path.expanduser(output_dir)
+        output_dir = os.path.join(output_dir, request.module.__name__)
+        mkdir(output_dir)
 
 @pytest.fixture(scope="function", autouse=True)
 def _auto_monkeypatch_send(sessions, request):
@@ -61,7 +75,7 @@ def _auto_rollback(sessions, request, sdconfig):
     backup = 'flash:snapshot-config-{}'.format(now)
 
     def _rollback():
-        responses = sessions.send(".*", "diff startup-config running-config")
+        responses = sessions.send("s?dut", "diff startup-config running-config")
 
         for resp in responses:
             diff_ = resp[0].output.strip()
@@ -70,25 +84,56 @@ def _auto_rollback(sessions, request, sdconfig):
                     'configure replace {}'.format(backup)
                 ])
 
-        response = sessions.send(".*", ['delete {}'.format(backup)])
+        response = sessions.send("s?dut", ['delete {}'.format(backup)])
 
     request.addfinalizer(_rollback)
 
-    response = sessions.send(".*", "copy running-config {}".format(backup))
+    response = sessions.send("s?dut", "copy running-config {}".format(backup))
 
-@pytest.fixture(scope="module", autouse=True)
-def _auto_backdoor(sessions, sdconfig, request):
+@pytest.fixture(scope="session", autouse=True)
+def _auto_backdoor(sessions, request):
 
-    def _rollback():
-        sessions.send(".*", [
-            "configure",
-            "no aaa root",
-            "end"
-        ])
-    request.addfinalizer(_rollback)
+    # def _rollback():
+    #     sessions.send(".*", [
+    #         "configure",
+    #         "no aaa root",
+    #         "end",
+    #         "write"
+    #     ])
+    # request.addfinalizer(_rollback)
 
-    sessions.send(".*", [
+    sessions.send("s?dut", [
         "configure",
         "aaa root secret root",
-        "end"
+        "end",
+        "write"
     ])
+
+@pytest.fixture(scope="session", autouse=True)
+def _auto_install_arstat(sessions, request):
+    filtered = sessions.filter(r"s?dut")
+
+    username, password = BACKDOOR_CREDENTIALS
+
+    dir = os.path.dirname(os.path.abspath(__file__))
+    path = "%s/../../../scripts/arstat" % dir
+
+    for sess in filtered:
+
+        scp = pexpect.spawn("scp %s %s@%s:/persist/local/arstat" % (path, username, sess.endpoint))
+        index = scp.expect(SSH_INIT_REGEX)
+        if index == 0:
+            scp.sendline("yes")
+            index = scp.expect(SSH_INIT_REGEX)
+
+        if index == 1:
+            scp.sendline(password)
+            scp.expect(pexpect.EOF)
+        scp.close()
+    #
+    #     ssh = pxssh.pxssh()
+    #     ssh.login(sess.endpoint, username="root", password="root")
+    #     ssh.sendline("chmod +x /persist/local/arstat")
+    #     ssh.prompt()
+    #     ssh.close()
+    # pass
