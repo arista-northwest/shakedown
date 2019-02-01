89b659e3e8543756cb8b9e3b51bcc00a4b46a4d1
Jesse Mather <jmather@arista.com>
2019-02-01 12:12:03 -0800

    add macsec, ip, ipv6, isis helpers

--- shakedown/scout/handlers/__init__.py
+++ shakedown/scout/handlers/__init__.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2018 Arista Networks, Inc.  All rights reserved.
 # Arista Networks, Inc. Confidential and Proprietary.
-from . import bgp, interfaces, system, lldp, lag, routes, macsec
+from . import bgp, interfaces, ip, ipv6, isis, lag, lldp, macsec, routes, \
+              system
 handlers = [
     ("system", system),
     ("interfaces", interfaces),
+    ("ip", ip),
+    ("ipv6", ipv6),
     ("bgp", bgp),
     ("lldp", lldp),
     ("lag", lag),
     ("routes", routes),
-    ("macsec", macsec)
+    ("macsec", macsec),
+    ("isis", isis)
 ]
