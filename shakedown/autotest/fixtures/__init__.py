b43325e69f7354d10558b0dc1e491b8c93f69f5e
Jesse Mather <jmather@arista.com>
2019-02-01 12:09:41 -0800

    cleaup imports

--- shakedown/autotest/fixtures/__init__.py
+++ shakedown/autotest/fixtures/__init__.py
@@ -2,7 +2,8 @@
 # Copyright (c) 2017 Arista Networks, Inc.  All rights reserved.
 # Arista Networks, Inc. Confidential and Proprietary.
 
-from .general import sdconfig, sessions, scout, testconfig, dut, sdut #, procplot
+from .general import sdconfig, sessions, scout, testconfig
+from .general import connections, dut, sdut, duts
 from .auto import _auto_monkeypatch_send, _auto_rollback, _auto_load_module
 from .auto import _auto_backdoor, _auto_install_arstat, _auto_output_dir
 from .reporting import sdreport, sdreportsection
