e729a50a083d4dc9097b4bccd578e6a05af2c7e3
Jesse Mather <jmather@arista.com>
2019-02-14 20:03:04 -0800

    reworked

--- shakedown/autotest/fixtures/backdoor.py
+++ shakedown/autotest/fixtures/backdoor.py
@@ -2,11 +2,11 @@
 
 
 import pytest
-from shakedown.backdoor import Backdoor
+from shakedown.backdoor import Session
 
 @pytest.fixture(scope="module")
 def backdoor(dut, request):
-    bkd = Backdoor()
+    bkd = Session()
     request.addfinalizer(lambda: bkd.close())
 
     bkd.open(dut.host)
