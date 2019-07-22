d37156652bbb797fe887903baa5a382f988303d8
Jesse Mather <jmather@arista.com>
2019-07-22 19:34:29 -0400

    add header

--- shakedown/autotest/fixtures/backdoor.py
+++ shakedown/autotest/fixtures/backdoor.py
@@ -1,5 +1,6 @@
-
-
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017 Arista Networks, Inc.  All rights reserved.
+# Arista Networks, Inc. Confidential and Proprietary.
 
 import pytest
 from shakedown.backdoor import Session
