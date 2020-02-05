104ff6c54343dc093e0fcfa386116c959fb0ef65
Jesse Mather <jmather@arista.com>
2020-02-04 16:11:24 -0800

    pull image from dockerhub

--- docker/jupyter/init.sh
+++ docker/jupyter/init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-CWD=$PWD
-cd /shakedown
-python3 setup.py develop
-cd $CWD
+# CWD=$PWD
+# cd /shakedown
+# python3 setup.py develop
+# cd $CWD
 
 jupyter notebook --NotebookApp.token='' --allow-root --port=8888 --no-browser --ip=0.0.0.0
