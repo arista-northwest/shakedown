e3e84d77115d75e67b88ae34d6ad05326c791535
Jesse Mather <jmather@arista.com>
2020-02-04 21:32:38 -0800

    formatting

--- shakedown/autotest/publishers/__init__.py
+++ shakedown/autotest/publishers/__init__.py
@@ -2,7 +2,5 @@
 # Copyright (c) 2018 Arista Networks, Inc.  All rights reserved.
 # Arista Networks, Inc. Confidential and Proprietary
 
-
-from . import json_ as json
-from . import markdown
-from . import html
+from shakedown.autotest.publishers import json_ as json
+from shakedown.autotest.publishers import mongodb, markdown, html
