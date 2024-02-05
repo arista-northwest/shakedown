62c085f54b048c38d84fda7f48e5b51b182f1f35
Jesse Mather <jmather@arista.com>
2024-02-05 18:31:32 +0000

    add minimal test case

---
+++ examples/tests/test_000_minimal.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2023 Arista Networks, Inc.  All rights reserved.
+# Arista Networks, Inc. Confidential and Proprietary.
+"""
+title: Minimal Collection
+
+description: |
+    Bare minimum
+
+settings:
+    auto_rollback: true
+"""
+
+def test_minimal(dut): ...
